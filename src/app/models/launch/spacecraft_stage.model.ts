edb00c15da9e2e6255317b319ffe57aacf9569f3
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2022-03-17 17:18:31 +0530

    removing errors

--- src/app/models/launch/spacecraft_stage.model.ts
+++ src/app/models/launch/spacecraft_stage.model.ts
@@ -4,7 +4,7 @@ export interface ISpacecraftStageModel {
     destination:string,
     launch_crew :IAstronautModel[],
     onboard_crew : IAstronautModel[],
-    landing_crew : IAstronautModel[] t,
+    landing_crew : IAstronautModel[],
     spacecraft: {
         id:number,
         name:string,
