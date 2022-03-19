736702761f3f51826f8272ba5fa24edcbcd6e90f
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2022-03-19 23:50:38 +0530

    formatting HTML TS files

--- src/app/components/launch/launch-detail/launch-detail.component.ts
+++ src/app/components/launch/launch-detail/launch-detail.component.ts
@@ -12,55 +12,55 @@ import { LaunchUtilService } from 'src/app/services/launch/launchUtil.service';
   templateUrl: './launch-detail.component.html',
   styleUrls: ['./launch-detail.component.css']
 })
-export class LaunchDetailComponent implements OnInit,OnDestroy {
+export class LaunchDetailComponent implements OnInit, OnDestroy {
 
   constructor(
-    private router:ActivatedRoute,
-    private sanitizer:DomSanitizer,
-    private launchService:LaunchService,
-    public launchUtilService:LaunchUtilService
-    ) { }
+    private router: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+    private launchService: LaunchService,
+    public launchUtilService: LaunchUtilService
+  ) { }
 
-  slug!:string|null;
+  slug!: string | null;
 
-  launchDetails!:ILaunchDetailModel;
-  videoURL :SafeResourceUrl | null=null;
+  launchDetails!: ILaunchDetailModel;
+  videoURL: SafeResourceUrl | null = null;
 
-  private launchServiceSubscription!:Subscription;
+  private launchServiceSubscription!: Subscription;
 
   ngOnInit(): void {
-    this.router.paramMap.subscribe((params:ParamMap)=>{
-      this.slug=params.get("slug");
+    this.router.paramMap.subscribe((params: ParamMap) => {
+      this.slug = params.get("slug");
 
-     this.launchServiceSubscription = this.launchService.getlaunchDetailsBySlug(this.slug!)
-     .subscribe({
-       next: data=>{         
-                    this.launchDetails=data;                     
-                    this.videoURL = data.vidURLs.length>0 ? this.createYoutubeEmbedURL(data.vidURLs[0].url):null; 
-                    },
-        error:err=>{
-          console.log(err,"error in launc-detail component");
-        }
+      this.launchServiceSubscription = this.launchService.getlaunchDetailsBySlug(this.slug!)
+        .subscribe({
+          next: data => {
+            this.launchDetails = data;
+            this.videoURL = data.vidURLs.length > 0 ? this.createYoutubeEmbedURL(data.vidURLs[0].url) : null;
+          },
+          error: err => {
+            console.log(err, "error in launc-detail component");
+          }
         })
     })
   }
 
-  ngOnDestroy():void{
+  ngOnDestroy(): void {
     this.launchServiceSubscription?.unsubscribe();
   }
 
-  private createYoutubeEmbedURL(url:string|null):SafeResourceUrl | null{  
-    if(url != null || url!=""){
-      
+  private createYoutubeEmbedURL(url: string | null): SafeResourceUrl | null {
+    if (url != null || url != "") {
+
       let tempURLArray = url!.split("watch?v=");
-      if(tempURLArray===undefined || tempURLArray===null || tempURLArray.length<=1){
-          return null;
+      if (tempURLArray === undefined || tempURLArray === null || tempURLArray.length <= 1) {
+        return null;
       }
-      else{
-          return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${tempURLArray[1].toString()}`);
-      }      
+      else {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${tempURLArray[1].toString()}`);
+      }
     }
-    else{
+    else {
       return null;
     }
   }
--- src/app/components/launch/launches/launches.component.ts
+++ src/app/components/launch/launches/launches.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute,ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { LaunchInfoModel } from 'src/app/models/launch/launchInfo.model';
 import { LaunchService } from 'src/app/services/launch/launch-service.service';
@@ -9,46 +9,46 @@ import { LaunchService } from 'src/app/services/launch/launch-service.service';
   templateUrl: './launches.component.html',
   styleUrls: ['./launches.component.css']
 })
-export class LaunchesComponent implements OnInit,OnDestroy {
+export class LaunchesComponent implements OnInit, OnDestroy {
 
-  constructor(private router:ActivatedRoute , 
-    private launchService:LaunchService) { }
+  constructor(private router: ActivatedRoute,
+    private launchService: LaunchService) { }
 
-  launchType!:string|null;
-  launchList:LaunchInfoModel[]=[];  
+  launchType!: string | null;
+  launchList: LaunchInfoModel[] = [];
 
-  private launchServiceSubscription!:Subscription;
+  private launchServiceSubscription!: Subscription;
 
-  ngOnInit(): void {  
-    this.router.paramMap.subscribe((params:ParamMap)=>{
-      this.launchType=params.get("launchType");
+  ngOnInit(): void {
+    this.router.paramMap.subscribe((params: ParamMap) => {
+      this.launchType = params.get("launchType");
       this.getlaunchesByType(this.launchType!);
-    })   
+    })
   }
 
-  ngOnDestroy():void{     
-    this.launchServiceSubscription?.unsubscribe();    
+  ngOnDestroy(): void {
+    this.launchServiceSubscription?.unsubscribe();
   }
 
-  private getlaunchesByType(launchType:string,filterType:string=""){
+  private getlaunchesByType(launchType: string, filterType: string = "") {
 
-    if(launchType==="upcoming"){
+    if (launchType === "upcoming") {
       this.launchServiceSubscription = this.launchService.getUpcomingLaunches(filterType)
-      .subscribe(data=>{
-        this.launchList = data;         
-      });
+        .subscribe(data => {
+          this.launchList = data;
+        });
     }
-    else if (launchType==="previous"){
+    else if (launchType === "previous") {
       this.launchServiceSubscription = this.launchService.getPreviousLaunches(filterType)
-      .subscribe(data=>{
-        this.launchList = data;
-      });
+        .subscribe(data => {
+          this.launchList = data;
+        });
     }
-    
+
   }
 
-  onFilterChange(selectEvent:any):void{    
-     this.getlaunchesByType(this.launchType!,selectEvent.target.value);
+  onFilterChange(selectEvent: any): void {
+    this.getlaunchesByType(this.launchType!, selectEvent.target.value);
   }
 
 }
